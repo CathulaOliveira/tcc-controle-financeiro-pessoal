0eb4ab80a19188fd3100720a47da00277a650718
cathula.oliveira <cathulapameladeoliveira@gmail.com>
2023-09-15 21:20:54 -0300

    schedule and whatsapp notification

--- server/src/main/java/br/edu/utfpr/notification/whatsapp/SendWhatsAppMessage.java
+++ server/src/main/java/br/edu/utfpr/notification/whatsapp/SendWhatsAppMessage.java
@@ -6,14 +6,14 @@ import com.twilio.type.PhoneNumber;
 
 public class SendWhatsAppMessage {
 
-    public static final String ACCOUNT_SID = "AC439ca355818b7d1a7068c438617ec11d";
-    public static final String AUTH_TOKEN = "aa3cb7af2f51197f8a53331d98fe82d9";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                        new PhoneNumber("whatsapp:+554691336424"),
-                        new PhoneNumber("whatsapp:+14155238886"),
+                        new PhoneNumber("whatsapp:+"), // numero real
+                        new PhoneNumber("whatsapp:+"), // numero da api
                         "Olá Cathula, vc não possui contas com vencimento próximo")
                 .create();
         System.out.println(message);
