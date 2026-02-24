fa18f808f8007df45cb277932ee7dd8e0dee4cfe
Cathula Oliveira <cathulapameladeoliveira@gmail.com>
2026-02-23 21:45:59 -0300

    Update SendWhatsAppMessage.java

--- server/src/main/java/br/edu/utfpr/notification/whatsapp/SendWhatsAppMessage.java
+++ server/src/main/java/br/edu/utfpr/notification/whatsapp/SendWhatsAppMessage.java
@@ -6,14 +6,14 @@ import com.twilio.type.PhoneNumber;
 
 public class SendWhatsAppMessage {
 
-    public static final String ACCOUNT_SID = "AC439ca355818b7d1a7068c438617ec11d";
-    public static final String AUTH_TOKEN = "aa3cb7af2f51197f8a53331d98fe82d9";
+    public static final String ACCOUNT_SID = "account_sid";
+    public static final String AUTH_TOKEN = "auth_token";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                        new PhoneNumber("whatsapp:+554691336424"), // numero real
-                        new PhoneNumber("whatsapp:+14155238886"), // numero da api
+                        new PhoneNumber("whatsapp:+numero"), // numero real
+                        new PhoneNumber("whatsapp:+api"), // numero da api
                         "Olá Cathula, sua transação recorrente Agua vence hoje")
                 .create();
         System.out.println(message);
